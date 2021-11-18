34663e0a12af4facbbaccd08cf97f36700e39c28
Cate Uch <cat.uch@yandex.ru>
2021-11-18 23:30:03 +0300

    props typing, React.FC

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,6 +1,13 @@
-export default function Todos () {
-    return <ul>
-        <li>Learn Rect</li>
-        <li>Learn Typescript</li>
+import React from "react";
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
-}
+  );
+};
+
+export default Todos;
