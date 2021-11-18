ed76dcbbe58f1d0c6b0ceefee1aafb44ba16bee9
Cate Uch <cat.uch@yandex.ru>
2021-11-18 23:43:44 +0300

    props typing by class

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Todo from './models/todo'
 import Todos from './components/Todos';
 import './App.css';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+
+  ]
+
   return (
     <div className="App">
-      <Todos items={['Learn React', 'Learn TypeScript']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
