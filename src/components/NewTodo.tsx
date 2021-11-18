987cc956c5da3bbae0e2f17cad426757836387a9
Cate Uch <cat.uch@yandex.ru>
2021-11-19 00:48:55 +0300

    form typing, useRef

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,13 @@
+
+
+const NewTodo = () => {
+  return (
+    <form>
+      <label htmlFor='text'>Todo text</label>
+      <input type="text" id='text'/>
+      <button>Add todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
